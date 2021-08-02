cb9ab9c842e56d7d044b4c4baa4e08a8c1f4d435
Shunji Zhan <shunhahaha@live.com>
2021-08-02 16:03:21 +0800

    added dummy event handlers

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Input,
   InputGroup,
@@ -17,17 +17,25 @@ import { FaUserAlt, FaLock, FaCheck } from "react-icons/fa";
 import social from "./assets/social.png";
 
 export default function SignIn() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(username, password);
+  }
+
   return (
-    <form>
+    <form onSubmit={ handleSubmit }>
       <Stack spacing="5">
-        <FormControl>
+        <FormControl >
           <InputGroup>
             <InputLeftAddon children={<FaUserAlt />} />
-            <Input placeholder="手机号或邮箱" />
+            <Input placeholder="手机号或邮箱" onChange={ e => setUsername(e.target.value) } />
           </InputGroup>
           <InputGroup>
             <InputLeftAddon children={<FaLock />} />
-            <Input type="password" placeholder="请输入密码" />
+            <Input type="password" placeholder="请输入密码" onChange={e => setPassword(e.target.value) } />
             <InputRightAddon children={<FaCheck />} />
           </InputGroup>
         </FormControl>
@@ -35,7 +43,7 @@ export default function SignIn() {
           <Switch id="deal" mr="3" />
           <FormLabel htmlFor="deal">记住登陆？</FormLabel>
         </Flex>
-        <Button _hover={{ bgColor: "#3194d0bb" }} w="100%" h="45px" borderRadius="20px" bgColor="#3194d0" color="white" padding="9px 18px">
+        <Button _hover={{ bgColor: "#3194d0bb" }} w="100%" h="45px" borderRadius="20px" bgColor="#3194d0" color="white" padding="9px 18px" type="submit">
           登录
         </Button>
         <Image src={social} w="100%" p="20px 0 0 0"/>
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Input,
   InputGroup,
@@ -15,25 +15,34 @@ import {
 import { FaUserAlt, FaLock, FaCheck, FaPhone } from "react-icons/fa";
 
 export default function SignUp() {
+  const [nickname, setNickname] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(nickname, username, password);
+  }
+
   return (
-    <form>
+    <form onSubmit={ handleSubmit }>
       <Stack spacing="5">
         <FormControl>
           <InputGroup>
             <InputLeftAddon children={<FaUserAlt />} />
-            <Input placeholder="请输入昵称" />
+            <Input placeholder="请输入昵称" onChange={ e => setNickname(e.target.value) } />
           </InputGroup>
           <InputGroup>
             <InputLeftAddon children={<FaPhone />} />
-            <Input placeholder="请输入用户名" />
+            <Input placeholder="请输入用户名" onChange={ e => setUsername(e.target.value) } />
           </InputGroup>
           <InputGroup>
             <InputLeftAddon children={<FaLock />} />
-            <Input type="password" placeholder="请输入密码" />
+            <Input type="password" placeholder="请输入密码" onChange={ e => setPassword(e.target.value) } />
             <InputRightAddon children={<FaCheck />} />
           </InputGroup>
         </FormControl>
-        <Button _hover={{ bgColor: "#42c021bb" }} w="100%" h="45px" borderRadius="20px" bgColor="#42c021" color="white" padding="9px 18px">
+        <Button _hover={{ bgColor: "#42c021bb" }} w="100%" h="45px" borderRadius="20px" bgColor="#42c021" color="white" padding="9px 18px" type="submit">
           登录
         </Button>
       </Stack>
